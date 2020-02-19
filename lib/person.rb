91456159a6154e30ff1e64833ddf577e6fff6516
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-02-19 04:34:35 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,4 +5,16 @@ class Person
     @this_person_name = name
   end
   
+  def name 
+    @this_person_name
+  end
+  
+  def job=(job)
+    @this_person_job = job
+  end
+  
+  def job 
+    @this_person_job
+  end
+  
 end

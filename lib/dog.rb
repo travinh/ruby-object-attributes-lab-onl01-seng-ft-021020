9dae83ced5724deeb2448a7d3c2ab7719709a582
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-02-19 04:33:14 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog 
+  
+  def name=(name)
+    @dog_name = name
+  end
+  
+  def name 
+    @dog_name
+  end
+  
+  def breed=(breed)
+    @dog_breed = breed
+  end
+  
+  def breed 
+    @dog_breed
+  end
+  
+end

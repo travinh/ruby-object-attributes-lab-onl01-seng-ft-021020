926423f1ef6cc8b03c53ce9d7a8c0bcfc0e05165
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-02-19 04:36:33 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog 
+  
+  def name=(dogname)
+    @name = dogname
+  end
+  
+  def name 
+    @name 
+  end
+  
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
+  
+  def breed 
+    @breed
+  end
+  
+end
---
+++ lib/person.rb
@@ -0,0 +1,20 @@
+class Person
+  
+  
+  def name=(this_person_name)
+    @name = this_person_name
+  end
+  
+  def name 
+    @name
+  end
+  
+  def job=(this_person_job)
+    @job = this_person_job
+  end
+  
+  def job 
+    @job
+  end
+  
+end
